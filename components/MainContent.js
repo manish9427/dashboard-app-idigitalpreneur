0dcddc7061c2564f57c926bd5e4f5cdc48d5362d
manish9427 <manish119427@gmail.com>
2025-09-04 18:08:34 +0530

    new

--- components/MainContent.js
+++ components/MainContent.js
@@ -161,7 +161,7 @@ export default function MainContent() {
           </div>
           <div className={styles.totalEarnings}>
             <h3>Total Earning</h3>
-            <ResponsiveContainer width="100%" height={200}>
+            <ResponsiveContainer width="100%" height="100%">
               <LineChart
                 data={revenueData}
                 margin={{ top: 20, right: 20, bottom: 20, left: 0 }}
